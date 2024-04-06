8f4131691347b4aeaf053070a4dd07137354efcf
dragon-ninja <166192949+dragon-ninja@users.noreply.github.com>
2024-04-06 11:43:25 +0800

    Delete Assets/Script/home/ui/Base directory

--- Assets/Script/home/ui/Base/UIType.cs
+++
@@ -1,9 +0,0 @@
-﻿
-public class UIType
-{
-    public UIformType ui_FormType = UIformType.Normal;
-    public UIformShowMode ui_ShowType = UIformShowMode.Normal;
-
-    //是否会清空UI栈
-    public bool IsClearStack = false;
-}
